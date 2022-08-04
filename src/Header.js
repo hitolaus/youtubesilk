3a021808f5ef70dc7f970a6730515550f977b790
Jakob Popa Hilarius Nielsen <jhn@storebox.com>
2022-08-04 16:27:25 +0200

    specify buildspec for codebuild

--- src/Header.js
+++ src/Header.js
@@ -7,14 +7,21 @@ function Header() {
     const navigate = useNavigate();
     const [ query, setQuery ] = useState();
 
+    async function goHome() {
+        navigate("/", { replace: true });
+    }
+
     async function handleSubmit(event) {
         event.preventDefault();
+        if (!query) {
+            return;
+        }
         navigate("/?q="+query, { replace: true });
       }
 
     return (
         <div className='header'>
-            <button onClick={() => navigate("/", { replace: true })}><MdHome size="2em"/></button>
+            <button onClick={() => goHome()}><MdHome size="2em"/></button>
             <form onSubmit={handleSubmit}>
                 <input placeholder='Search' onChange={(e) => setQuery(e.target.value)} />
                 <button><MdSearch size="2em"/></button>
--- src/VideoList.js
+++ src/VideoList.js
@@ -9,7 +9,7 @@ function VideoList(props) {
     const [ searchParams ] = useSearchParams();
     const [ videos, setVideos ] = useState([]);
 
-    let q = searchParams.get('q') || 'disney';  // TODO
+    let q = searchParams.get('q') || 'kids';
 
     useEffect(() => {
         let params =  {
@@ -19,7 +19,7 @@ function VideoList(props) {
             regionCode: 'RO',
             relevanceLanguage: 'ro',
             safeSearch: 'strict',
-            maxResults: 6,
+            maxResults: 20,
             videoEmbeddable: true, // Can be embedded
             videoSyndicated: true, // Can be played outsite youtube.com
             q: q
@@ -32,18 +32,45 @@ function VideoList(props) {
 
         if (process.env.REACT_APP_YOUTUBE_API_MOCK === 'true') {
             axios.get('mock/list.json').then((response) => {
-                setVideos(response.data);
+                setVideos(filter(response.data, 6));
             });    
 
             }
             else {
                 axios.get('https://www.googleapis.com/youtube/v3/search', { params: params })
                 .then((response) => {
-                    setVideos(response.data?.items);
+                    setVideos(filter(response.data?.items, 6));
                 });
             }
         
-    }, [props.related, props.q, q]);
+            const filter = (items, count) => {
+                if (!items) {
+                    return items;
+                }
+                return items
+                    .filter((item) => {
+                        
+                        
+                        const blacklistedChannel = isBlacklisted(item.snippet?.channelTitle);
+                        const blacklistedTitle = isBlacklisted(item.snippet?.title);
+                        const blacklistedDescription = isBlacklisted(item.snippet?.description);
+        
+                        return !(blacklistedChannel ||  blacklistedTitle || blacklistedDescription); 
+                    })
+                    .splice(0, count);
+            };
+            
+            const isBlacklisted = (str) => {
+                if (!str) {
+                    return false;
+                }
+        
+                const blacklistKeywords = [ 'diana', 'roma' ];
+        
+                return blacklistKeywords.some(e => { return str.toLowerCase().split(/\s+/).includes(e.toLocaleLowerCase());});
+            };
+    }, [props.related, q]);
+
 
 
     return (
